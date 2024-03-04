2412a890a778b64ac49eb7501582142866dc2aba
nguyentiem <nguyentiem21101998@gmail.com>
2024-03-05 00:17:09 +0700

    add file

---
+++ test_file_system.h
@@ -0,0 +1,14 @@
+#ifndef _FILE_SYSTEM_H_
+#define _FILE_SYSTEM_H_
+#include <zephyr/drivers/gpio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int test_fs(void); 
+
+#ifdef __cplusplus
+}
+#endif
+#endif
